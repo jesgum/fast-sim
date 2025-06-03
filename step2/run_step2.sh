74361c619127ec4e74fc4856e1642345d2a7d236
jesgum <jesper.gumprecht@gmail.com>
2025-06-03 14:37:26 +0200

    add external generator

--- step2/run_step2.sh
+++ step2/run_step2.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-NEVENTS=1000
+NEVENTS=10000
 CONFIGURATION="-b --configuration json://configuration.json "
 TIMEFRAME=300
 
 
 o2-sim-dpl-eventgen --generator pythia8pp --nEvents ${NEVENTS} ${CONFIGURATION} --aggregate-timeframe ${TIMEFRAME} \
 | o2-sim-mctracks-to-aod ${CONFIGURATION} \
-| o2-analysis-onthefly-tracker ${CONFIGURATION}
+| o2-analysis-onthefly-tracker ${CONFIGURATION} \
+| o2-analysis-alice3-decaypreselector ${CONFIGURATION} \
+| o2-analysis-alice3-decayfinder ${CONFIGURATION}
