bf8451c1e8005c859bf98146bf8df431cc438ac4
jesgum <jesper.gumprecht@gmail.com>
2025-06-03 15:18:01 +0200

    fix type

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Hands-on: Fast simulations
 
 ## Step 0 -- Getting started
-Preliminary step to make sure that pur look-up tables valid and are located in the correct folder
+Preliminary step to make sure that our look-up tables valid and are located in the correct folder
 
 ## Step 1 -- Fast analysis
 We try to run a simple analysis task with minimal configurations
---
+++ step4/run_step4.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+NEVENTS=10000
+CONFIGURATION="-b --configuration json://configuration.json "
+TIMEFRAME=300
+GENERATOR_CONFIG="../external_generator/ini/pythia8_pp.ini"
+
+o2-sim-dpl-eventgen --generator external --nEvents ${NEVENTS} ${CONFIGURATION} ${GENERATOR_CONFIG}
+--aggregate-timeframe ${TIMEFRAME} \
+| o2-sim-mctracks-to-aod ${CONFIGURATION} \
+| o2-analysis-onthefly-tracker ${CONFIGURATION} \
+| o2-analysis-alice3-decaypreselector ${CONFIGURATION} \
+| o2-analysis-alice3-decayfinder ${CONFIGURATION}
