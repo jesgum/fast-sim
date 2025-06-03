b10238b06046270ecf2dd9d37a3841f1d4c18ca8
jesgum <jesper.gumprecht@gmail.com>
2025-06-03 17:18:17 +0200

    Update step3

--- step3/run_step3.sh
+++ step3/run_step3.sh
@@ -3,11 +3,10 @@
 NEVENTS=10000
 CONFIGURATION="-b --configuration json://configuration.json "
 TIMEFRAME=300
-GENERATOR_CONFIG="../external_generator/ini/pythia8_pp.ini"
+GENERATOR_CONFIG="--configFile ../external_generator/ini/pythia8_pp.ini"
 
-o2-sim-dpl-eventgen --generator external --nEvents ${NEVENTS} ${CONFIGURATION} ${GENERATOR_CONFIG}
---aggregate-timeframe ${TIMEFRAME} \
+o2-sim-dpl-eventgen --generator external --nEvents ${NEVENTS} ${CONFIGURATION} ${GENERATOR_CONFIG} --aggregate-timeframe ${TIMEFRAME} \
 | o2-sim-mctracks-to-aod ${CONFIGURATION} \
 | o2-analysis-onthefly-tracker ${CONFIGURATION} \
-| o2-analysis-alice3-decaypreselector ${CONFIGURATION} \
-| o2-analysis-alice3-decayfinder ${CONFIGURATION}
+| o2-analysis-onthefly-tofpid ${CONFIGURATION} \
+| o2-analysis-onthefly-richpid ${CONFIGURATION}
